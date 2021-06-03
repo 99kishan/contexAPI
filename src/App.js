5b63dfef35ef99543ebb810b4003979fc1233002
Kishan Bhamidipati <kishan99.b@gmail.com>
2021-06-03 17:44:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalContext';
+
+
+function App() {
+  return (
+    <div className='box'>
+    <GlobalProvider>
+      <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+
+export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((num, item) => (num += item), 0).toFixed(2);
+
+    return (
+        <div>
+            <h4>Current Balance</h4>
+            <h1 >₹{total}</h1>
+        </div>
+    )
+}
